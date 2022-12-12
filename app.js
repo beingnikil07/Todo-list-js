2e7038ad0ca26ad5ad2d6913f8fbd141d2e10885
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-12 06:03:56 -0500

    getting the input value

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const item=document.querySelector('#item')
-const todoBox=document.querySelector('#todo-box')
+const item = document.querySelector("#item");
+const todoBox = document.querySelector("#todo-box");
 
-       // *****Below code shows you what actually what keyup event does ****** 
+/* 
+     *****Below code shows you what actually what keyup event does ****** 
 item.addEventListener("keyup",function(event){    
-    console.log(event);
+    console.log(event.key);
 });
 
+*/
 
-
-
-
-
+item.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    console.log(this.value); //Here ,This refers to the item id that is the id of input box
+    this.value = "";
+  }
+});
