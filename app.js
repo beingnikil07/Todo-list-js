c3b605af9734a5241d952d83fbefd30e2d0cce13
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-12 05:36:21 -0500

    code shows you what keyup event actually does

---
+++ app.js
@@ -0,0 +1,13 @@
+const item=document.querySelector('#item')
+const todoBox=document.querySelector('#todo-box')
+
+       // *****Below code shows you what actually what keyup event does ****** 
+item.addEventListener("keyup",function(event){    
+    console.log(event);
+});
+
+
+
+
+
+
